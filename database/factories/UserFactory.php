3773dd6c8f698b5f1e200674ef069b063781e36d
Douglas Leslie <lesliedouglas23@gmail.com>
2023-03-24 09:20:58 +0100

    feat: write test using phpunit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -15,14 +16,26 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
+    /**
+     * @var User
+     */
+    private $user;
+
     public function definition(): array
     {
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'email_verified' => 1,
         ];
     }
 
